b3afe49a2c43ac4b20f6b5906575a1309aa778bf
Kan <kanyakancat@gmail.com>
2023-07-24 15:06:06 +0300

    8 cart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.css'
 import './scss/app.scss'
 import { Header } from './components/Header'
 import { Home } from './pages/Home'
@@ -7,21 +7,18 @@ import { Route, Routes } from 'react-router-dom'
 import { Cart } from './pages/Cart'
 
 function App() {
-  
-  return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <div className="container">
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/cart' element={<Cart />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className='wrapper'>
+			<Header />
+			<div className='content'>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/cart' element={<Cart />} />
+					<Route path='*' element={<NotFound />} />
+				</Routes>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,31 +5,33 @@ import { Skeleton } from '../components/PizzaBlock/Skeleton'
 import { PizzaBlock } from '../components/PizzaBlock/PizzaBlock'
 
 export function Home() {
-	  const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] = useState(true);
-  
-  // [] - первый рендер
-  useEffect(() => {
-    fetch('https://64bbb2af7b33a35a44469688.mockapi.io/items')
-      .then((res) => res.json())
-      .then((json) => {
-        setItems(json)
-        setIsLoading(false)
-      })
-  }, [])
-	
+	const [items, setItems] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
+
+	// [] - первый рендер
+	useEffect(() => {
+		fetch('https://64bbb2af7b33a35a44469688.mockapi.io/items')
+			.then(res => res.json())
+			.then(json => {
+				setItems(json)
+				setIsLoading(false)
+			})
+		window.scrollTo(0, 0)
+	}, [])
+
 	return (
 		<>
-			<div className="content__top">
-				<Categories />
-				<Sort />
-			</div>
-			<h2 className="content__title">Все пиццы</h2>
-			<div className="content__items">
-				{isLoading
-					? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-					: items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)
-				}
+			<div className='container'>
+				<div className='content__top'>
+					<Categories />
+					<Sort />
+				</div>
+				<h2 className='content__title'>Все пиццы</h2>
+				<div className='content__items'>
+					{isLoading
+						? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+						: items.map(pizza => <PizzaBlock key={pizza.id} {...pizza} />)}
+				</div>
 			</div>
 		</>
 	)
