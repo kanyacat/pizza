8d3313dadffb9b53297f94430836a602860bbc24
Kan <kanyakancat@gmail.com>
2023-07-26 16:40:53 +0300

    10/2 input

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,63 @@
+import styles from './Search.module.scss'
+
+export function Search(props) {
+	const { searchValue, setSearchValue } = props
+	return (
+		<>
+			<div className={styles.root}>
+				<svg
+					className={styles.icon}
+					width='800px'
+					height='800px'
+					viewBox='0 0 32 32'
+					version='1.1'
+					xmlns='http://www.w3.org/2000/svg'
+				>
+					<title>search</title>
+					<desc>Created with Sketch Beta.</desc>
+					<defs></defs>
+					<g
+						id='Page-1'
+						stroke='none'
+						strokeWidth='1'
+						fill='none'
+						fillRule='evenodd'
+					>
+						<g
+							id='Icon-Set'
+							transform='translate(-256.000000, -1139.000000)'
+							fill='#000000'
+						>
+							<path
+								d='M269.46,1163.45 C263.17,1163.45 258.071,1158.44 258.071,1152.25 C258.071,1146.06 263.17,1141.04 269.46,1141.04 C275.75,1141.04 280.85,1146.06 280.85,1152.25 C280.85,1158.44 275.75,1163.45 269.46,1163.45 L269.46,1163.45 Z M287.688,1169.25 L279.429,1161.12 C281.591,1158.77 282.92,1155.67 282.92,1152.25 C282.92,1144.93 276.894,1139 269.46,1139 C262.026,1139 256,1144.93 256,1152.25 C256,1159.56 262.026,1165.49 269.46,1165.49 C272.672,1165.49 275.618,1164.38 277.932,1162.53 L286.224,1170.69 C286.629,1171.09 287.284,1171.09 287.688,1170.69 C288.093,1170.3 288.093,1169.65 287.688,1169.25 L287.688,1169.25 Z'
+								id='search'
+							></path>
+						</g>
+					</g>
+				</svg>
+				<input
+					value={searchValue}
+					className={styles.input}
+					onChange={event => setSearchValue(event.target.value)}
+					placeholder='Поиск пиццы...'
+				/>
+				{searchValue && (
+					<svg
+						onClick={() => setSearchValue('')}
+						className={styles.clearIcon}
+						width='800px'
+						height='800px'
+						viewBox='0 0 24 24'
+						fill='none'
+						xmlns='http://www.w3.org/2000/svg'
+					>
+						<path
+							d='M20.7457 3.32851C20.3552 2.93798 19.722 2.93798 19.3315 3.32851L12.0371 10.6229L4.74275 3.32851C4.35223 2.93798 3.71906 2.93798 3.32854 3.32851C2.93801 3.71903 2.93801 4.3522 3.32854 4.74272L10.6229 12.0371L3.32856 19.3314C2.93803 19.722 2.93803 20.3551 3.32856 20.7457C3.71908 21.1362 4.35225 21.1362 4.74277 20.7457L12.0371 13.4513L19.3315 20.7457C19.722 21.1362 20.3552 21.1362 20.7457 20.7457C21.1362 20.3551 21.1362 19.722 20.7457 19.3315L13.4513 12.0371L20.7457 4.74272C21.1362 4.3522 21.1362 3.71903 20.7457 3.32851Z'
+							fill='#0F0F0F'
+						/>
+					</svg>
+				)}
+			</div>
+		</>
+	)
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,9 @@ import { Sort } from '../components/Sort'
 import { Skeleton } from '../components/PizzaBlock/Skeleton'
 import { PizzaBlock } from '../components/PizzaBlock/PizzaBlock'
 
-export function Home() {
+export function Home(props) {
+	const { searchValue } = props
+
 	const [items, setItems] = useState([])
 	const [isLoading, setIsLoading] = useState(true)
 	const [categoryId, setCategoryId] = useState(0)
@@ -18,12 +20,12 @@ export function Home() {
 		setIsLoading(true)
 
 		const category = categoryId > 0 ? `category=${categoryId}` : ''
-
 		const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
 		const sortBy = sortType.sortProperty.replace('-', '')
+		const search = searchValue ? `&search=${searchValue}` : ''
 
 		fetch(
-			`https://64bbb2af7b33a35a44469688.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+			`https://64bbb2af7b33a35a44469688.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`
 		)
 			.then(res => res.json())
 			.then(json => {
@@ -31,7 +33,19 @@ export function Home() {
 				setIsLoading(false)
 			})
 		window.scrollTo(0, 0)
-	}, [categoryId, sortType])
+	}, [categoryId, sortType, searchValue])
+
+	const pizzas = items.map(pizza => <PizzaBlock key={pizza.id} {...pizza} />)
+	/* фильтр если статичный список
+	.filter(pizza => {
+		if (pizza.title.toLowerCase().includes(searchValue.toLowerCase())) {
+			return true
+		} else return false
+	}) */
+
+	const skeletons = [...new Array(6)].map((_, index) => (
+		<Skeleton key={index} />
+	))
 
 	return (
 		<>
@@ -44,11 +58,7 @@ export function Home() {
 					<Sort value={sortType} onChangeSort={id => setSortType(id)} />
 				</div>
 				<h2 className='content__title'>Все пиццы</h2>
-				<div className='content__items'>
-					{isLoading
-						? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-						: items.map(pizza => <PizzaBlock key={pizza.id} {...pizza} />)}
-				</div>
+				<div className='content__items'>{isLoading ? skeletons : pizzas}</div>
 			</div>
 		</>
 	)
